ea8fd2752f708fd74d9e64f5657d146d3fe4303b
devferx <ferkki@hotmail.com>
2022-01-08 21:25:54 -0400

    :sparkles: [ADD] Routing con Express.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 100,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
