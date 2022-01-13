e2a8035a0d4d4ad009ea623fc4443a2400dd5d45
devferx <ferkki@hotmail.com>
2022-01-13 17:11:05 -0400

    :fire: [ADD] Products PUT, PATCH y DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -35,9 +35,41 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
+
   res.json({
     message: 'created',
     data: body,
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    message: 'deleted',
+    id,
   });
 });
 
