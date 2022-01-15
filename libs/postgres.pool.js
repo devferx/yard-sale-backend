faa16ced0fadb353f78e2b35d5e91ee9bd3ba01b
devferx <ferkki@hotmail.com>
2022-01-15 10:33:19 -0400

    Variables de ambiente en Node.js

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
 
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'devferx',
-  password: 'admin123',
-  database: 'my_store',
+  connectionString: URI,
 });
 
 module.exports = pool;
