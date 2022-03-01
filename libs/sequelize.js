7d5d47b547eb98c8300240c7edf910a76dfa754b
devferx <ferkki@hotmail.com>
2022-03-01 12:05:09 -0400

    :wrench: [ADD] Sequelize cli config

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,8 +5,8 @@ const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
   dialect: 'mysql',
@@ -15,6 +15,9 @@ const sequelize = new Sequelize(URI, {
 
 setupModels(sequelize);
 
+// Lee los modelos y comienza a crear las tablas
+// Es muy delicado y no se aconseja utilizar en Producción
+// Debido a que esta sobreescribiendo la base de datos
 sequelize.sync();
 
 module.exports = sequelize;
