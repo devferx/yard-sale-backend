fcb1d6c24911522de01774e8dc03967f935d3390
devferx <ferkki@hotmail.com>
2022-03-05 17:26:07 -0400

    Protección de rutas

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 
 const CategoryService = require('./../services/category.service');
 const validatorHandler = require('./../middlewares/validator.handler');
@@ -36,6 +37,7 @@ router.get(
 
 router.post(
   '/',
+  passport.authenticate('jwt', { session: false }),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
