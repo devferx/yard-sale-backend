e7c33d545124290088bf4121a556c94238532841
devferx <ferkki@hotmail.com>
2023-01-26 15:46:42 -0400

    Validar roles

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,13 +1,13 @@
-const boom = require('@hapi/boom');
 const CategoryService = require('./../services/category.service');
+const { checkRolesGql } = require('../utils/checkRolesGql');
+const { checkJwtGql } = require('../utils/checkJwtGql');
 
 const service = new CategoryService();
 
 const addCategory = async (_, { dto }, context) => {
-  const { user } = await context.authenticate('jwt', { session: false });
-  if (!user) {
-    throw boom.unauthorized('jwt is not valid');
-  }
+  const user = await checkJwtGql(context);
+
+  checkRolesGql(user, 'admin', 'customer');
 
   return service.create(dto);
 };
