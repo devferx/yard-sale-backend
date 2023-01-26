b7cb42993ea2b611d2733e6c507ab7be11a62cd0
devferx <ferkki@hotmail.com>
2023-01-26 10:51:07 -0400

    Listas y non-null types

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -9,26 +9,28 @@ const { expressMiddleware } = require('@apollo/server/express4');
 // POST, PUT, DELETE = Mutation
 const typeDefs = `
   type Query {
-    hello: String
+    hello: String!
     getPerson(name: String, age: Int): String
-    getInt: Int
+    getInt(age: Int!): Int
     getFloat: Float
     getString: String
     getBoolean: Boolean
     getID: ID
+    getNumbers(numbers: [Int!]!): [Int]
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => 'Hello world from Apollo Server',
     getPerson: (_, args) =>
       `Hello, my name is ${args.name}, I'm ${args.age} years old`,
-    getInt: () => 123,
+    getInt: (_, args) => args.age,
     getFloat: () => Math.random() * 100,
     getString: () => 'Hello world',
     getBoolean: () => Math.random() > 0.5,
     getID: () => '1234567890',
+    getNumbers: (_, args) => args.numbers,
   },
 };
 
