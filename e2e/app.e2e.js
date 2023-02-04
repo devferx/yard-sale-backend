13d966722505898037880e6189666de0acc21b0e
devferx <ferkki@hotmail.com>
2023-02-04 12:19:49 -0400

    Pruebas a rutas con protección

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,11 +1,15 @@
 const request = require('supertest');
 
 const createApp = require('../src/app');
+const { config } = require('../src/config/config');
 
 describe('test for app', () => {
   let app = null;
   let server = null;
-  let api = request(null);
+  /**
+   * @type {import('supertest').SuperTest<import('supertest').Test>}
+   */
+  let api = null;
 
   beforeAll(async () => {
     app = await createApp();
@@ -24,5 +28,25 @@ describe('test for app', () => {
     expect(response.status).toEqual(200);
     expect(response.body.name).toEqual('Fernando');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  describe('GET /nueva-ruta', () => {
+    test('Should return 401', async () => {
+      const { statusCode } = await api.get('/nueva-ruta');
+
+      expect(statusCode).toEqual(401);
+    });
+
+    test('Should return 401 with invalid apiKey', async () => {
+      const { statusCode } = await api.get('/nueva-ruta').set({ api: '1212' });
+      expect(statusCode).toEqual(401);
+    });
+
+    test('Should return 200', async () => {
+      const { statusCode } = await api
+        .get('/nueva-ruta')
+        .set({ api: config.apiKey });
+      expect(statusCode).toEqual(200);
+    });
   });
 });
