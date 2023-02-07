fe504e05dabec14315443f37a773bf8825195fc6
devferx <ferkki@hotmail.com>
2023-02-07 10:56:34 -0400

    Pruebas a la paginación

--- e2e/products.e2e.js
+++ e2e/products.e2e.js
@@ -25,6 +25,28 @@ describe('test for /products path', () => {
       expect(body.length).toEqual(products.length);
       expect(body[0].category).toBeTruthy();
     });
+
+    test('should return 2 products with limit = 2 and offset = 0', async () => {
+      const limit = 2;
+      const offset = 0;
+      const { statusCode, body } = await api.get(
+        `/api/v1/products?limit=${limit}&offset=${offset}`
+      );
+
+      expect(statusCode).toEqual(200);
+      expect(body.length).toEqual(limit);
+    });
+
+    test('should return 2 products with limit = 2 and offset = 2', async () => {
+      const limit = 2;
+      const offset = 2;
+      const { statusCode, body } = await api.get(
+        `/api/v1/products?limit=${limit}&offset=${offset}`
+      );
+
+      expect(statusCode).toEqual(200);
+      expect(body.length).toEqual(limit);
+    });
   });
 
   afterAll(async () => {
